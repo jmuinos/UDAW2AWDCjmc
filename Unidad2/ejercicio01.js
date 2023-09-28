672c8d95dd84c5cf4a6784750759d744de79c333
José <josemuinhos@gmail.com>
2023-09-29 00:45:50 +0200

    Ejercicios Unidad 2 -> terminados

---
+++ Unidad2/ejercicio01.js
@@ -0,0 +1,13 @@
+// Declaración de las variables
+let entero = 1357;
+let decimal = 135.7;
+let cientifico = 135e7;
+let octal = 0o1357;
+let hexadecimal = 0x1357;
+
+// Mostrar en consola
+console.log("Número entero " + entero);
+console.log("Número decimal " + decimal);
+console.log("Número científico " + cientifico);
+console.log("Número octal " + octal);
+console.log("Número hexadecimal " + hexadecimal);
