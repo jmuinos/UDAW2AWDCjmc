ba25b90b6f8a3205ec3f6a2bfa665c0a117cccce
José <josemuinhos@gmail.com>
2023-09-29 01:26:23 +0200

    Tarea 02 -> terminada

---
+++ MuinosJoseUnidad2_Tarea/desplazarBits.js
@@ -0,0 +1,15 @@
+// 125 / 8 = 125 >> 3
+console.log("\nResultado de 125 / 8 usando desplazamiento de bits:");
+console.log(125 >> 3);
+
+// 40 x 4 = 40 << 2
+console.log("\nResultado de 40 x 4 usando desplazamiento de bits:");
+console.log(40 << 2);
+
+// 25 / 2 = 25 >> 1
+console.log("\nResultado de 25 / 2 usando desplazamiento de bits:");
+console.log(25 >> 1);
+
+// 10 x 16 = 10 << 4
+console.log("\nResultado de 10 x 16 usando desplazamiento de bits:");
+console.log(10 << 4);
---
+++ MuinosJoseUnidad2_Tarea/tablasMultiplicar.js
@@ -0,0 +1,21 @@
+// Tabla de multiplicar del 7 usando bucle for
+console.log("Tabla de multiplicar del 7:");
+for (let i = 1; i <= 10; i++) {
+    console.log(`7 x ${i} = ${7 * i}`);
+}
+
+// Tabla de sumar del 8 usando bucle while
+console.log("\nTabla de sumar del 8:");
+let j = 1;
+while (j <= 10) {
+    console.log(`8 + ${j} = ${8 + j}`);
+    j++;
+}
+
+// Tabla de dividir del 9 usando bucle do...while
+console.log("\nTabla de dividir del 9:");
+let k = 1;
+do {
+    console.log(`9 / ${k} = ${9 / k}`);
+    k++;
+} while (k <= 10);
